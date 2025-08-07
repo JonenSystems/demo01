eae35df3f24f61e0f54c7b7b52665f42c57b68f3
JonenSystems <yoshidago@oburabike.com>
2025-08-08 01:57:53 +0900

    フッター変更

--- shopping/src/main/java/com/example/shopping/config/WebConfig.java
+++ shopping/src/main/java/com/example/shopping/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.shopping.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,25 +11,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /**
-     * 静的リソースの設定
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // CSSファイルの設定
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/css/");
+        /**
+         * 静的リソースの設定
+         */
+        @Override
+        public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+                // CSSファイルの設定
+                registry.addResourceHandler("/css/**")
+                                .addResourceLocations("classpath:/static/css/");
 
-        // JavaScriptファイルの設定
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/");
+                // JavaScriptファイルの設定
+                registry.addResourceHandler("/js/**")
+                                .addResourceLocations("classpath:/static/js/");
 
-        // 画像ファイルの設定
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/", "file:/opt/shopping/static/images/");
+                // 画像ファイルの設定
+                registry.addResourceHandler("/images/**")
+                                .addResourceLocations("classpath:/static/images/", "file:/opt/shopping/static/images/");
 
-        // その他の静的リソース
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-    }
+                // その他の静的リソース
+                registry.addResourceHandler("/**")
+                                .addResourceLocations("classpath:/static/");
+        }
 }
